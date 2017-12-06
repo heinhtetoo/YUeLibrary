7f8647d17a0cd419311ae23adf63527ad5206fe1
heinhtetoo <heinhtetoo.mmr@gmail.com>
2017-12-07 01:36:40 +0630

    Refix png crunch error

--- app/src/main/java/heinhtetoo/yuelibrary/activities/BookDetailActivity.java
+++ app/src/main/java/heinhtetoo/yuelibrary/activities/BookDetailActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
